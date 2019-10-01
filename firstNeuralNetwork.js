799da93869dbde0221055535f67d33c79849376d
Tobias <sullytobias@gmail.com>
2019-10-01 11:20:15 +0200

    Basic neural network

--- firstNeuralNetwork.js
+++ firstNeuralNetwork.js
@@ -1,4 +1,17 @@
 const BrainJS = require('Brain.js');
 
+const neuralNetwork = new BrainJS.NeuralNetwork({ });
 
+const data = [
+  { input: [0, 0], output: [0] },
+  { input: [0, 1], output: [1] },
+  { input: [1, 0], output: [1] },
+  { input: [1, 1], output: [0] }
+];
 
+neuralNetwork.train(data, {
+  log: error => console.log(error),
+  logPeroid: 100
+});
+
+console.log(neuralNetwork.run([1, 0]));
